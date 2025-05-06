a9e702bd0c63983f18da6d8a0a0beeb0e45e036f
Topot <fran653@hotmail.com>
2025-05-06 20:33:52 +0200

    ?

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -21,8 +21,8 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name ='home'),
-    path("about/", views.about, name='about'),
-    path("portfolio/", views.portfolio, name='portfolio'),
-    path("contact/", views.contact, name='contacttt'),
-    path("admin/", admin.site.urls),
+    path('about/', views.about, name='about'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('contact/', views.contact, name='contact'),
+    path('admin/', admin.site.urls),
 ]
